f40bfd83890dbf537f6f035cc94b4f57f910a860
zhiquan_yu <2894789761@qq.com>
2016-07-27 13:37:12 +0800

    fix linter

--- src/presentationals/ToolBar/index.jsx
+++ src/presentationals/ToolBar/index.jsx
@@ -9,14 +9,18 @@ const ToolBar = ({ ...other }) => (
   <div className={styles.container}>
     <button className={styles.showOrHideLibraryBtn} />
 
-    <button className={styles.toolBarBtn} title="add a blank line, then write your word">P</button>
     <button className={styles.toolBarBtn} title="# add your h1 here">H1</button>
     <button className={styles.toolBarBtn} title="## add your h2 here">H2</button>
     <button className={styles.toolBarBtn} title="### add your h3 here">H3</button>
     <button className={styles.toolBarBtn} title="#### add your h4 here">H4</button>
     <button className={styles.toolBarBtn} title="##### add your h5 here">H5</button>
     <button className={styles.toolBarBtn} title="###### add your h6 here">H6</button>
-    <button className={styles.toolBarBtn} title="** add your strong here **"><strong>B</strong></button>
+    <button
+      className={styles.toolBarBtn}
+      title="** add your strong here **"
+    >
+      <strong>B</strong>
+    </button>
     <button className={styles.toolBarBtn} title="_ add your em here _"><em>I</em></button>
     <button className={styles.toolBarBtn} title="---">Hr</button>
 
@@ -24,8 +28,18 @@ const ToolBar = ({ ...other }) => (
     <button className={styles.toolBarBtn} title="* add your ul here">Ul</button>
     <button className={styles.toolBarBtn} title="&gt; add your blockquote here">Blockquote</button>
 
-    <button className={styles.toolBarBtn} title="[add your link title here](link here)">Link</button>
-    <button className={styles.toolBarBtn} title="[add your image alt here](image link here)">Image</button>
+    <button
+      className={styles.toolBarBtn}
+      title="[add your link title here](link here)"
+    >
+      Link
+    </button>
+    <button
+      className={styles.toolBarBtn}
+      title="[add your image alt here](image link here)"
+    >
+      Image
+    </button>
     <button className={styles.toolBarBtn} title="` add your code here `">Code</button>
 
     <button className={styles.showOrHideToolBarBtn} />

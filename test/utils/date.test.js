71833430a7df7ad63fe95ca2a43f91c61cb5f3de
zhiquan_yu <2894789761@qq.com>
2016-08-01 16:09:32 +0800

    Add string util test

--- test/utils/date.test.js
+++ test/utils/date.test.js
@@ -2,8 +2,8 @@ import { expect } from 'chai'
 
 import { getyyyymmddhhMMss } from '../../src/utils/date'
 
-describe('getyyyymmddhhMMss 函数测试', () => {
-  it('getyyyymmddhhMMss(new Date("2016-08-01 14:46:00")) 应该得到字符串 2016-08-01 14:46:00', () => {
+describe('test util date', () => {
+  it('getyyyymmddhhMMss(new Date("2016-08-01 14:46:00")) should be "2016-08-01 14:46:00"', () => {
     expect(getyyyymmddhhMMss(new Date('2016-08-01 14:46:00'))).to.be.equal('2016-08-01 14:46:00')
   })
 })
